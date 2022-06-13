71adf19dfc09b4152fa602a8e9f83b0becc017da
eich1cyfaill <hu4lifefllwer@gmail.com>
2022-06-13 19:13:01 +0300

    fk back to browser router

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import {BrowserRouter, HashRouter} from 'react-router-dom';
+import {BrowserRouter} from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './store/indexStore';
 
@@ -10,9 +10,9 @@ import { store } from './store/indexStore';
 
 
 ReactDOM.render(
-    <HashRouter>
+    <BrowserRouter>
         <Provider store={store}><App /></Provider>
-    </HashRouter>,
+    </BrowserRouter>,
 
   document.getElementById('root')
 );
